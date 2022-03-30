ab58e0bae68f22ab036420a676856035f51cfe1a
Davide De Leonardis <davidedeleonardis1711@gmail.com>
2022-03-30 13:10:59 +0200

    add 20 real users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,55 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            [
-                'photo' => '',
-                'name' => 'Ristorante da Davide',
-                'email' => 'davidedeleonardis1711@gmail.com',
-                'password' => '12345678',
-                'address' => 'Piazza Umberto 47',
-                'phone' => '33353364734',
-                'p_iva' => '12345678911',
-            ],
-            [
-                'photo' => '',
-                'name' => 'Ristorante da Manuel',
-                'email' => 'manuelcappello7@gmail.com',
-                'password' => '12345678',
-                'address' => 'Piazza Umberto 47',
-                'phone' => '33353364732',
-                'p_iva' => '12345898911',
-            ],
-            [
-                'photo' => '',
-                'name' => 'Ristorante da Christian',
-                'email' => 'christian@gmail.com',
-                'password' => '12345678',
-                'address' => 'Piazza Umberto 47',
-                'phone' => '33353364731',
-                'p_iva' => '12367678911',
-            ],
-            [
-                'photo' => '',
-                'name' => 'Ristorante da Semola',
-                'email' => 'semola@gmail.com',
-                'password' => '12345678',
-                'address' => 'Piazza Umberto 47',
-                'phone' => '33353364737',
-                'p_iva' => '12455678911',
-            ],
-            [
-                'photo' => '',
-                'name' => 'Ristorante da Dario',
-                'email' => 'dario@gmail.com',
-                'password' => '12345678',
-                'address' => 'Piazza Umberto 47',
-                'phone' => '33353364738',
-                'p_iva' => '12345238911',
-            ],
-        ];
-
-        foreach ($users as $user) {
+        foreach (config('users') as $user) {
             $newUser = new User();
             $newUser->photo = $user['photo'];
             $newUser->name = $user['name'];
