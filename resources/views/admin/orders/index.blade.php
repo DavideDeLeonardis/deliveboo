7b57c02c16f28b770bfabb2fdada22ceec36aa73
dariomontani <dariomontani@gmail.com>
2022-04-01 16:38:59 +0200

    index orders, index users style

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.admin')
 
 @section('content')
-    <ul>
-        <?php $i = 1 ?>
-        @foreach ($orders as $order)
-            <h5 class="mt-3">Ordine {{$i}}
-            </h5>
-            <li>{{ $order->name }}</li>
-            <li>{{ $order->lastname }}</li>
-            <li>{{ $order->email }}</li>
-            <li>{{ $order->address }}</li>
-            <li>{{ $order->price_total }} &euro;</li>
-            <li>{{ $order->date }}</li>
-            <li>{{ $order->time}}</li>
-            <li><a class="btn btn-primary" href="{{ route('admin.orders.show', $order)}}">Visualizza ordine</a></li>
-            <?php $i++ ?>
-        @endforeach
-    </ul>
+    <div class="container bg-dark">
+        <?php $i = 1; ?>
+        <div class="row d-flex justify-content-around">
+            @foreach ($orders as $order)
+                <div class="col m-2">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Ordine {{ $i }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Nome: {{ $order->name }} {{ $order->lastname }}
+                            </h6>
+                            <p class="card-text">Email: {{ $order->email }}</p>
+                            <p class="card-text">Indirizzo: {{ $order->address }}</p>
+                            <p class="card-text">Totale: {{ $order->price_total }} &euro;</p>
+                            <p class="card-text">Data: {{ $order->date }}</p>
+                            <p class="card-text">Ora: {{ $order->time }}</p>
+                            <a class="btn btn-light text-light" style="background-color: #38c172 !important"
+                                href="{{ route('admin.orders.show', $order) }}">Visualizza
+                                ordine</a>
+                        </div>
+                    </div>
+                    <?php $i++; ?>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
