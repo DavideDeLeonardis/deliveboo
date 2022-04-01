93a983feb0e1d72e30a0917be86995d8882408a1
dariomontani <dariomontani@gmail.com>
2022-04-01 12:29:46 +0200

    modifiche img

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-8 text-white my_bg-dark">
                 <div class="w-25 p-2">
-                    <img class="img-fluid" src="../../../public/images/20220322_170706.png" alt="DeliveBoo">
+                    <img class="img-fluid" src="../../images/20220322_170706.png" alt="DeliveBoo">
                 </div>
             </div>
             <div class="col-4 p-2 d-flex align-items-center flex-row-reverse my_bg-dark">
