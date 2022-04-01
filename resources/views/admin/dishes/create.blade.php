a85deeebc83767667996f7ef30c0e68963b56fe9
dariomontani <dariomontani@gmail.com>
2022-04-01 10:23:27 +0200

    create

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h1 class="">Aggiungi nuovo Piatto:</h1>
+    <div class="container bg-dark">
+        <h1 class="text-light" style="color: #08a88a !important">
+            <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json" trigger="loop"
+                colors="primary:#e8b730,secondary:#08a88a" style="width:80px;height:50px">
+            </lord-icon>Aggiungi un nuovo Piatto:
+        </h1>
         <div class="row">
             <div class="col py-3">
                 <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
@@ -11,9 +15,12 @@
 
                     {{-- nome --}}
                     <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" required value="{{ old('name') }}"
-                            data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right">
+                        <label for="name" class="form-label text-light">
+                            <h4>Nome del Piatto:</h4>
+                        </label>
+                        <input type="text" class="form-control" style="background-color: #e8b730" id="name" name="name"
+                            required value="{{ old('name') }}" data-bs-toggle="tooltip" data-bs-placement="right"
+                            title="Tooltip on right">
                         @error('name')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -21,9 +28,11 @@
 
                     {{-- descrizione --}}
                     <div class="mb-3">
-                        <label for="description" class="form-label">Description</label>
-                        <textarea type="text" class="form-control" id="description" name="description" rows="3"
-                            placeholder="Opzionale..">{{ old('description') }}</textarea>
+                        <label for="description" class="form-label text-light">
+                            <h4>Descrizione:</h4>
+                        </label>
+                        <textarea type="text" class="form-control" style="background-color: #e8b730" id="description" name="description"
+                            rows="3" placeholder="Opzionale..">{{ old('description') }}</textarea>
                         @error('description')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -31,8 +40,10 @@
 
                     {{-- immagine --}}
                     <div class="mb-3">
-                        <label for="image" class="form-label">Image</label>
-                        <input class="form-control" type="file" id="image" name='image'>
+                        <label for="image" class="form-label text-light">
+                            <h4>Immagine:</h4>
+                        </label>
+                        <input class="form-control" style="background-color: #e8b730" type="file" id="image" name='image'>
                         @error('image')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -40,19 +51,23 @@
 
                     {{-- ingredienti --}}
                     <div class="mb-3">
-                        <label for="ingredients" class="form-label">Ingredients</label>
-                        <textarea type="text" class="form-control" id="ingredients" name="ingredients" rows="3"
-                            placeholder="Opzionale..">{{ old('ingredients') }}</textarea>
+                        <label for="ingredients" class="form-label text-light">
+                            <h4>Ingredienti:</h4>
+                        </label>
+                        <textarea type="text" class="form-control" style="background-color: #e8b730" id="ingredients" name="ingredients"
+                            rows="3" placeholder="Opzionale..">{{ old('ingredients') }}</textarea>
                         @error('ingredients')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
                     </div>
 
-                    {{-- prezzo  --}}
+                    {{-- prezzo --}}
                     <div class="mb-3">
-                        <label for="price" class="form-label">Price</label>
-                        <input type="text" class="form-control" id="price" name="price" required
-                            value="{{ old('price') }}">
+                        <label for="price" class="form-label text-light">
+                            <h4>Prezzo:</h4>
+                        </label>
+                        <input type="text" class="form-control" style="background-color: #e8b730" id="price" name="price"
+                            required value="{{ old('price') }}">
                         @error('price')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -60,8 +75,8 @@
 
                     {{-- portata --}}
                     <div class="mb-3">
-                        <h2>Scegli Portata:</h2>
-                        <select class="form-select" name="course">
+                        <h4 class="text-light">Scegli Portata:</h4>
+                        <select class="form-select" style="background-color: #e8b730" name="course">
                             @foreach ($courses as $course)
                                 <option value="{{ $course }}" @if (old('course')) selected @endif>
                                     {{ $course }}</option>
@@ -74,8 +89,8 @@
 
                     {{-- disponibilità --}}
                     <div class="mb-3">
-                        <h2>Disponibilità:</h2>
-                        <select class="form-select" name="availability">
+                        <h4 class="text-light">Disponibilità:</h4>
+                        <select class="form-select" style="background-color: #e8b730" name="availability">
                             <option value="0" @if (old('availability')) selected @endif>Non Disponibile</option>
                             <option value="1" @if (old('availability')) selected @endif>Disponibile</option>
                             @error('availability')
@@ -84,9 +99,10 @@
                         </select>
                     </div>
 
-                    <button type="submit" class="btn btn-danger">Save</button>
+                    <button type="submit" class="btn btn-success" style="background-color: #08a88a">Salva il Piatto</button>
                 </form>
             </div>
         </div>
     </div>
+    <script src="https://cdn.lordicon.com/lusqsztk.js"></script>
 @endsection
