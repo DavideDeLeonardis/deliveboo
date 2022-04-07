4da9081c6c90e3e6a2b321e8a4a2d8102697ad5d
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-04-07 17:51:27 +0200

    first form control

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\PaymentRequest;
 use Braintree\Gateway;
 use App\Model\Dish;
+use Carbon\Carbon;
+use App\Model\Order;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -51,5 +53,45 @@ class PaymentController extends Controller
             ];
             return response()->json($data, 404);
         }
+    }
+
+    public function order(Request $request) {
+
+        $request->validate([
+            'name' => 'required | max:100',
+            'lastname' => 'required | max:100',
+            'address' => 'required | max:100',
+            'email' => 'required | email | max:100',
+            'cart' => 'required'
+        ]);
+
+        $data = $request->all();
+        
+        // dd(json_decode(request('cart')));
+        // $cart = (array)json_decode($data['cart']);
+        // var_dump($data['cart']);
+
+        $newOrder = new Order();
+        $newOrder->payment_id = 2;
+        $newOrder->name = $data['name'];
+        $newOrder->lastname = $data['lastname'];
+        $newOrder->email = $data['email'];
+        $newOrder->address = $data['address'];
+        $newOrder->date = Carbon::now()
+            ->format('Y-m-d');
+        $newOrder->time = Carbon::now()
+            ->format('H:m');
+        $newOrder->price_total = 100;
+
+        foreach ($data['cart'] as $dish) {
+            $newOrder->dishes()->attach($dish, ['quantity' => $dish->quantity]);
+            // $newOrder->dishes()->order_id = $newOrder->id;
+            // $newOrder->dishes()->quantity = $dish->quantity;
+        }
+        
+        $newOrder->save();
+        
+        
+
     }
 }
