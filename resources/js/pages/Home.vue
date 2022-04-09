36f952ac946b4d932e984fe2e74ca6401daa6f73
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-04-09 19:34:22 +0200

    fix comment home

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -141,7 +141,7 @@ export default {
         //clear localStorage when back to the home
         this.$store.state["cart"] = [];
         localStorage.clear();
-        console.log(this.$store.state['location'].href)
+        // console.log(this.$store.state['location'].href)
         // if (JSON.parse(localStorage.getItem("location"))) {
         //     alert('Scegliendo un altro ristorante rispetto al precedente, perderai il carrello')
         // }
