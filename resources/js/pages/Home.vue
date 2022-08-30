759fda729efd76e7bfd21a869c8e38baf7cba7bc
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-30 18:31:34 +0100

    commit

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -159,7 +159,7 @@ export default {
     data() {
         return {
             loading: false,
-            url: "http://pure-falls-97096.herokuappppp.com/api/v1/",
+            url: "http://pure-falls-97096.herokuapp.com/api/v1/",
             restaurants: null,
             categories: [],
             pages: {
