9fda2540be011d045fcc3bcb8d13d343fb0bb39c
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-31 12:21:07 +0100

    c

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -77,17 +77,17 @@
                 <div class="ms-3 me-4">
                     <span>CREDENTIALS</span>
                     <div class="d-flex">
-                        <div>
-                            <div class="ms-3"><u>Email:</u> &nbsp; pasticceriafloro@gmail.com</div>
-                            <div class="ms-3"><u>Password:</u> &nbsp; 12345678</div>
+                        <div class="mx-3">
+                            <div><u>Email:</u> &nbsp; pasticceriafloro@gmail.com</div>
+                            <div><u>Password:</u> &nbsp; 12345678</div>
+                        </div>
+                        <div class="me-3">
+                            <div><u>Email:</u> &nbsp; botta@gmail.com</div>
+                            <div><u>Password:</u> &nbsp; 12345678</div>
                         </div>
                         <div>
-                            <div class="ms-3"><u>Email:</u> &nbsp; botta@gmail.com</div>
-                            <div class="ms-3"><u>Password:</u> &nbsp; 12345678</div>
-                        </div>
-                        <div>
-                            <div class="ms-3"><u>Email:</u> &nbsp; 44gattibistrot@gmail.com</div>
-                            <div class="ms-3"><u>Password:</u> &nbsp; 12345678</div>
+                            <div><u>Email:</u> &nbsp; 44gattibistrot@gmail.com</div>
+                            <div><u>Password:</u> &nbsp; 12345678</div>
                         </div>
                     </div>
                 </div>
